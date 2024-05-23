e2db5c93b57ac4ff0e47047fef2c95bf934d7570
minjilee <budlalswl1129@gmail.com>
2024-05-23 16:56:13 +0900

    feat: 알림창 & 선택메뉴 컴포넌트 구현

---
+++ src/components/ChoiceMenu.tsx
@@ -0,0 +1,33 @@
+import React, { FC, ReactNode, useState } from 'react';
+import { IoCloseOutline } from 'react-icons/io5';
+
+interface IProps {
+  title: string;
+  children: ReactNode;
+  onClose: () => void;
+}
+
+export const ChoiceMenu: FC<IProps> = ({ title, children, onClose }) => {
+  return (
+    <>
+      <div
+        className='fixed flex flex-col items-center justify-center bg-black bg-opacity-50 w-[500px] h-full z-10'
+        onClick={onClose}
+      ></div>
+      <div className='absolute z-20 bottom-0 bg-white w-full h-auto pt-3 pb-10 border border-none rounded-t-[3rem]'>
+        <div className='flex h-20 items-center px-7'>
+          <IoCloseOutline
+            size={28}
+            className='cursor-pointer'
+            onClick={onClose}
+          />
+          <p className='font-hanaRegular text-[1.8rem] w-full text-center mr-10'>
+            {title}
+          </p>
+        </div>
+        <hr className='mt-0.5 divide-hanaGray' />
+        <div className='px-7 mt-7 h-full'>{children}</div>
+      </div>
+    </>
+  );
+};
--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+    </>
+  );
 };
