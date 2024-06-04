0ace8325aab9c68215661e57fed1ced6a0dfcca1
EunSo0 <dmstn423@naver.com>
2024-06-04 09:46:14 +0900

    design: 랜딩페이지 애니메이션 추가

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCookie } from '../utils/cookie';
 import { useNavigate } from 'react-router-dom';
 
 export const Landing = () => {
   const navigate = useNavigate();
+  const [visible, setVisible] = useState<boolean>(false);
 
   useEffect(() => {
+    const animate = setTimeout(() => {
+      setVisible(true);
+    }, 1500);
     const timeout = setTimeout(() => {
       if (getCookie('phone')) {
         navigate('/home');
@@ -13,16 +17,31 @@ export const Landing = () => {
         navigate('/join');
       }
     }, 3000);
-    return () => clearTimeout(timeout);
+    return () => {
+      clearTimeout(animate);
+      clearTimeout(timeout);
+    };
   }, []);
 
   return (
     <div className='bg-white h-screen flex flex-col justify-center items-center'>
-      <img src='/images/별돌이logo.svg' className='w-48' />
-      <div className='font-hanaBold text-5xl mb-2 text-hanaRed'>
-        똑똑<span className='text-hanaGreen'>하나</span>
+      <img src='/images/bulb.png' className='w-16 animate-flyinup' />
+      <img src='/images/별돌이logo.svg' className='w-48 z-20' />
+      <div className='flex'>
+        <div className='font-hanaBold text-5xl mb-2 text-hanaRed animate-tada'>
+          똑
+        </div>
+        <div className='font-hanaBold text-5xl mb-2 text-hanaRed animate-tada'>
+          똑
+        </div>
+        <div className='font-hanaBold text-5xl mb-2 text-hanaGreen '>하</div>
+        <div className='font-hanaBold text-5xl mb-2 text-hanaGreen '>나</div>
       </div>
-      <div className='font-hanaLight text-2xl text-hanaGreen'>
+
+      <div
+        className={`font-hanaLight text-2xl text-hanaGreen animate-fadeinbounceup
+      ${!visible && 'opacity-0'}`}
+      >
         <span className='text-hanaRed font-hanaMedium'>재</span>테크{' '}
         <span className='text-hanaRed font-hanaMedium'>습</span>관{' '}
         <span className='text-hanaRed font-hanaMedium'>기</span>르기
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,12 +34,70 @@ export default {
             opacity: 1,
           },
         },
+        'fade-in-bounceup': {
+          '0%': {
+            opacity: 0,
+            transform: 'translate3d(0%, 100%, 0)',
+          },
+          '33%': {
+            opacity: 0.5,
+            transform: 'translate3d(0%, 0%, 0)',
+          },
+          '66%': {
+            opacity: 0.7,
+            transform: 'translate3d(0%, 20%, 0)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translate3d(0, 0, 0)',
+          },
+        },
+        tada: {
+          '0%': {
+            transform: 'scale3d(1, 1, 1)',
+          },
+          '10%, 20%': {
+            transform: 'scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -6deg)',
+          },
+          '30%, 50%, 70%, 90%': {
+            transform: 'scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 6deg)',
+          },
+          '40%, 60%, 80%': {
+            transform: 'scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -6deg)',
+          },
+          '100%': {
+            transform: 'scale3d(1, 1, 1)',
+          },
+        },
+        'fly-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translate3d(0, 50px, 0)',
+            transitionTimingFunction: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+          },
+          '60%': {
+            opacity: '1',
+            transform: 'translate3d(0, -20px, 0)',
+          },
+          '75%': {
+            transform: 'translate3d(0, 10px, 0)',
+          },
+          '90%': {
+            transform: 'translate3d(0, -5px, 0)',
+          },
+          '100%': {
+            transform: 'translate3d(0, 0, 0)',
+          },
+        },
       },
       animation: {
         fadein1: 'fade-in 1.5s ease-in-out 0.5s 1',
         fadein2: 'fade-in 1.5s ease-in-out 1s 1',
         fadein3: 'fade-in 1.5s ease-in-out 1.5s 1',
         fadein4: 'fade-in 1.5s ease-in-out 2s 1',
+        fadeinbounceup: 'fade-in-bounceup 1s ease-in-out 1s 1',
+        tada: 'tada 1s ease-in-out 0.25s 1',
+        flyinup: 'fly-in-up 0.6s ease-in-out 0.25s 1',
       },
     },
     minHeight: {
